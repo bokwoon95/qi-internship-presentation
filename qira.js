4d5fc65f187f5a4c02079e3e2802317e6cd3868f
bokwoon <bokwoon.c@gmail.com>
2019-07-30 14:57:29 +0800

    completed

--- qira.js
+++ qira.js
@@ -2,7 +2,7 @@
 
 let draggedElement = null;
 
-function elDragstart(event) {
+function dragstart(event) {
   const el = event.currentTarget;
   setTimeout(function() {
     el.classList.add("el-ghost");
@@ -10,33 +10,39 @@ function elDragstart(event) {
   draggedElement = el;
 }
 
-function elDragend(event) {
+function dragend(event) {
   const el = event.currentTarget;
   el.classList.remove("el-ghost");
   draggedElement = null;
 }
 
-function elDragover(event) {
-  const el = event.currentTarget;
-  const parent = el.parentNode;
-  const boundingRect = el.getBoundingClientRect();
-  if (!draggedElement) {
-    return;
-  }
-  if (el === draggedElement) {
-    return;
-  } else if (el === parent.firstElementChild) {
-    parent.insertBefore(draggedElement, el);
-  } else if (el === parent.lastElementChild) {
-    parent.insertBefore(draggedElement, el.nextElementSibling);
-  } else if (el.nextElementSibling === draggedElement) {
-    parent.insertBefore(draggedElement, el);
-  } else if (el === draggedElement.nextElementSibling) {
-    parent.insertBefore(draggedElement, el.nextElementSibling);
-  } else if (event.pageY - boundingRect.bottom < boundingRect.top - event.pageY) {
-    parent.insertBefore(draggedElement, el);
-  } else {
-    parent.insertBefore(draggedElement, el.nextElementSibling);
+function dragover(event) {
+  const parent = event.currentTarget;
+  const target = event.target;
+  const isParent = target => target.classList.contains("sortable");
+  const isChild = target => target.classList.contains("sortable-el");
+  if (isParent(target)) {
+    parent.appendChild(draggedElement);
+  } else if (isChild(target)) {
+    const boundingRect = target.getBoundingClientRect();
+    if (!draggedElement) {
+      return;
+    }
+    if (target === draggedElement) {
+      return;
+    } else if (target === parent.firstElementChild) {
+      parent.insertBefore(draggedElement, target);
+    } else if (target === parent.lastElementChild) {
+      parent.insertBefore(draggedElement, target.nextElementSibling);
+    } else if (target.nextElementSibling === draggedElement) {
+      parent.insertBefore(draggedElement, target);
+    } else if (target === draggedElement.nextElementSibling) {
+      parent.insertBefore(draggedElement, target.nextElementSibling);
+    } else if (event.pageY - boundingRect.bottom < boundingRect.top - event.pageY) {
+      parent.insertBefore(draggedElement, target);
+    } else {
+      parent.insertBefore(draggedElement, target.nextElementSibling);
+    }
   }
 }
 
@@ -46,6 +52,7 @@ window.onload = function() {
     sortable.classList.add("pl-4");
     sortable.classList.add("pr-4");
     sortable.classList.add("pb-4");
+    sortable.addEventListener("dragover", dragover);
     for (const sortableEl of sortable.children) {
       sortableEl.setAttribute("draggable", "true");
       sortableEl.classList.add("sortable-el");
@@ -53,9 +60,8 @@ window.onload = function() {
       sortableEl.classList.add("min-h-16");
       sortableEl.classList.add("h-auto");
       sortableEl.classList.add("bg-gray-500");
-      sortableEl.addEventListener("dragstart", elDragstart);
-      sortableEl.addEventListener("dragend", elDragend);
-      sortableEl.addEventListener("dragover", elDragover);
+      sortableEl.addEventListener("dragstart", dragstart);
+      sortableEl.addEventListener("dragend", dragend);
     }
   }
 };
